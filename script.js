c8a69d2014f12caddeec0c020b433eb4963451c0
LU1ZK4M4R0VSK1 <kamarovskiwork@gmail.com>
2024-12-26 09:44:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    $(document).ready(function () {
+        $(".carousel-wrapper").owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            items: 1
+        })
+    });    
+</script>
